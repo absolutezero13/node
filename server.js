051dc14c19865c738176749eca412cb53dadcabb
ilker <ilker.sarica@wunder.is>
2021-05-12 22:11:04 +0300

    error handling

--- server.js
+++ server.js
@@ -1,3 +1,17 @@
+// HANDLING ERRORS GLOBALLY, NOT RELATED TO EXPRESS
+
+process.on('unhandledRejection', err => {
+  console.log('Unhandled kekw ! shuw down!');
+  console.log(err.name);
+  server.close(() => process.exit(1));
+});
+
+process.on('uncaughtException', err => {
+  console.log(err.name);
+
+  server.close(() => process.exit(1));
+});
+
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
