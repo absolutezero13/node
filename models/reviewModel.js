ed1aea131694270875614346a87169c9eed94635
absolutezero13 <ilker.sarica@wunder.is>
2021-06-27 14:27:06 +0300

    ratings average

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const Tour = require('./tourModel');
 
 const reviewSchema = new mongoose.Schema(
   {
@@ -36,6 +37,29 @@ reviewSchema.pre(/^find/, function(next) {
     select: 'name photo'
   });
   next();
+});
+
+reviewSchema.statics.calcAverageRatings = async function(tour) {
+  const stats = await this.aggregate([
+    {
+      $match: { tour }
+    },
+    {
+      $group: {
+        _id: '$tour',
+        nRating: { $sum: 1 },
+        avgRating: { $avg: '$rating' }
+      }
+    }
+  ]);
+  await Tour.findByIdAndUpdate(tour, {
+    ratingsQuantity: stats[0].nRating,
+    ratingsAverage: stats[0].avgRating
+  });
+};
+
+reviewSchema.post('save', function() {
+  this.constructor.calcAverageRatings(this.tour);
 });
 
 const Review = mongoose.model('Review', reviewSchema);
